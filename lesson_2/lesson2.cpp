8c0eafb2908dbbcf1461e373f6023534e659edec
Andrei <nedav@mail.ru>
2021-02-21 19:44:14 +0300

    reinit master

---
+++ lesson_2/lesson2.cpp
@@ -0,0 +1,14 @@
+/*
+* Домашнее задание к уроку 1
+* курса "Основы языка С++"
+* автор Недокунев А.В.
+* среда разработки Notepad++ /компилятор MinGW/
+* codepage OEM866
+*/
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
